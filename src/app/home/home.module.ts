c7995cc4eec8b2cb023c8f9e3e7deb93484b0137
Alejandro Bernal <alejandro.bernal@urbantxt.com>
2021-07-22 12:22:23 -0700

    Firt commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { NgCalendarModule } from 'ionic2-calendar';
+import { CalModalPageModule } from '../pages/cal-modal/cal-modal.module';
+
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCalendarModule,
+    ReactiveFormsModule,
+    CalModalPageModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  // providers: [{ provide: LOCALE_ID, useValue: 'en-US' }],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,138 @@
-import { Component } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+import { CalModalPage } from '../pages/cal-modal/cal-modal.page';
+
+// I have addeded an interface to represent user data when creating an event
+// interface Event {
+//   title: string;
+//   startTime: string;
+//   endTime: string;
+//   allDay: boolean;
+// }
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild(CalendarComponent) myCal: CalendarComponent;
+  public eventSource = [];
+  public viewTitle: string;
 
-  constructor() {}
+  // Model the event and store it in the list
+  public event: Event;
+  public eventList = [];
+  public calendar = {
+    mode: 'day',
+    currentDate: new Date(),
+  };
+  public selectedDate: Date;
 
+  constructor(
+    private alertController: AlertController,
+    // @Inject(LOCALE_ID) private locale: string,
+    private modalController: ModalController
+  ) {}
+
+  ngOnInit() {}
+
+  next() {
+    this.myCal.slideNext();
+  }
+
+  back() {
+    this.myCal.slidePrev();
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  async openCalModal() {
+    const modal = await this.modalController.create({
+      component: CalModalPage,
+      cssClass: 'cal-modal',
+      backdropDismiss: false,
+    });
+
+    await modal.present();
+
+    modal.onDidDismiss().then((result) => {
+      if (result.data && result.data.event) {
+        let event = result.data.event;
+        if (event.allDay) {
+          let start = event.startTime;
+          event.startTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate()
+            )
+          );
+          event.endTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate() + 1
+            )
+          );
+        }
+        this.eventSource.push(result.data.event);
+        this.myCal.loadEvents();
+      }
+    });
+  }
+
+  // createAnEvent() {
+  // create values for both start and end time
+  // let startTime;
+  // let endTime;
+  // const date = new Date();
+  // Used to classify start or end time
+  // let eventType = Math.floor(Math.random() * 2);
+  // define a startDay
+  // let startDay = Math.floor(Math.random() * 90) - 45;
+  // let endDay = Math.floor(Math.random() * 2) + startDay;
+
+  // startTime = new Date(
+  //   Date.UTC(
+  //     date.getUTCFullYear(),
+  //     date.getUTCMonth(),
+  //     date.getUTCDate() + startDay
+  //   )
+  // );
+
+  // if (endDay === startTime) {
+  //   endDay += 1;
+  // }
+
+  // endTime = new Date(
+  //   Date.UTC(
+  //     date.getUTCFullYear(),
+  //     date.getUTCMonth(),
+  //     date.getUTCDate() + endDay
+  //   )
+  // );
+
+  // this.event = {
+  //   title: 'this is a test',
+  //   startTime,
+  //   endTime,
+  //   allDay: false,
+  // };
+
+  // this.eventList.push(this.event);
+  // this.eventSource = this.eventList;
+
+  // console.log(date, '<==== Value of DATE');
+  // console.log(eventType, '<==== Event Type Value');
+  //   console.log(startDay, '<==== Value of StartDay');
+  //   console.log(endDay, '<==== Value of End Of Day');
+  //   console.log(startTime, '<==== Our first start time');
+  //   console.log(endTime, '<==== Our first end time');
+  //   console.log(this.eventList, '<===== our created list');
+  // }
 }
